baa20c28cc9acfd921a04838a3550b8822c0b366
Golden_key <golden.key@qq.com>
2023-11-06 11:56:37 +0900

    Update homework 4

--- 2023_fall/lec04/homework4.py
+++ 2023_fall/lec04/homework4.py
@@ -5,7 +5,11 @@ def list_to_dict(input_list):
     `input_list` is a value, and the corresponding key is the numerical 
     index of that element in `input_list`. 
     '''
-    pass
+    new_dict = {}
+    for i in range(len(input_list)):
+        new_dict[i] = input_list[i]
+        
+    return new_dict
 
 
 
